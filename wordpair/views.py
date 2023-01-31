83800dac3cff81b4059965fa707f8f1776993665
evangelav <evangelav16@gmail.com>
2023-01-31 11:33:25 +0100

    bug fix for getALL method

--- wordpair/views.py
+++ wordpair/views.py
@@ -1,14 +1,15 @@
-import json
+import sqlite3
 
-from rest_framework.renderers import JSONRenderer
 from rest_framework.decorators import api_view
-from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework import status
 
 from wordpair.models import Word
 from wordpair.serializers import WordSerializer
+from pathlib import Path
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Create your views here.
 @api_view(['GET', 'POST'])
@@ -16,7 +17,9 @@ def word_list(request, format=None):
     if request.method == 'GET':
         words = Word.objects.all()
         serializer = WordSerializer(words, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        #embedded with words : [{...},{...}]
+        data = {"words" : serializer.data}
+        return Response(data, status=status.HTTP_200_OK)
     elif request.method == 'POST':
         serializer = WordSerializer(data=request.data)
         if serializer.is_valid():
@@ -27,16 +30,12 @@ def word_list(request, format=None):
 @api_view(['GET', 'DELETE', 'PUT'])
 def word_detail(request, pk, format=None):
 
-    try:
-        word = Word.objects.get(pk=pk)
-    except Word.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
+    word = get_word(pk=pk)
     if request.method == 'PUT':
         serializer = WordSerializer(word, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)            
+            return Response(serializer.data, status=status.HTTP_200_OK)           
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
     elif request.method == 'DELETE':
         word.delete()
@@ -44,3 +43,11 @@ def word_detail(request, pk, format=None):
     elif request.method == 'GET':
         serializer = WordSerializer(word)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+def get_word(pk):
+    try:
+        word = Word.objects.get(pk=pk)
+        return word
+    except Word.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
